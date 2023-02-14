634acf4f28e53fffbc798fd199569e4740dc7bfb
Sinan Sonmez (Chaush) <37421564+sinansonmez@users.noreply.github.com>
2023-02-14 23:31:13 +0100

    write post create student business logic

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,10 @@ public class StudentService {
   }
 
   public void addNewStudent(Student student) {
+    Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+    if (studentOptional.isPresent()) {
+      throw new IllegalStateException("email taken");
+    }
     studentRepository.save(student);
   }
 
